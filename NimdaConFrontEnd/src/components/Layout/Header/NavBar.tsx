a17d0767b412563433d292fda4ec06482d0df00e
nuyoes <s471y@kakao.com>
2025-09-10 23:41:46 +0900

    chore. 경로 및 폴더명 변경

--- NimdaConFrontEnd/src/components/Layout/Header/NavBar.tsx
+++ NimdaConFrontEnd/src/components/Layout/Header/NavBar.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import BrandSection from '@/components/icons/Logo';
+import Logo from '@/components/icons/Logo';
 import UserActions from './UserName';
 
 const NavBar: React.FC = () => {
   return (
     <nav className="fixed top-0 left-0 right-0 z-50 h-16 border-b border-[#E0E0E0] bg-white">
       <div className="h-full px-4 xl:px-6 max-w-7xl mx-auto flex items-center justify-between">
-        <BrandSection />
-        <UserActions />
+        <Logo />
+
       </div>
     </nav>
   );
