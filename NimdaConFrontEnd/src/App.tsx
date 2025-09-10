68fae2c576ec634e2c60e942d75c341378208032
nuyoes <s471y@kakao.com>
2025-09-10 20:49:55 +0900

    feat. 버튼 생성 및 라우팅

--- NimdaConFrontEnd/src/App.tsx
+++ NimdaConFrontEnd/src/App.tsx
@@ -1,13 +1,24 @@
 import './App.css'
+import Button from '@/components/Button/Black'
 import NavBar from './components/Layout/NavBar/NavBar'
+import { useNavigate } from "react-router-dom";
 
 function App() {
+  const navigate = useNavigate();
+
+  const goToLogIn = () => {
+    navigate("/login");
+  };
+
+
   return (
     /* w-full : full width */
     <div className="w-full">
       <NavBar />
       <main className="p-8">
-        Test
+        <Button onClick={goToLogIn}>
+        버튼버튼
+      </Button>
       </main>
     </div>
   )
