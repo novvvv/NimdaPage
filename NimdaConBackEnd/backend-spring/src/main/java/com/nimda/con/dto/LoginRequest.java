a11f7e11fb0e8b980d7f2343589766113768bf52
nov <novslog@gmail.com>
2025-09-10 23:38:45 +0900

    Fix: Login DTO 수정, 아이디, 패스워드 길이제한 설정

--- NimdaConBackEnd/backend-spring/src/main/java/com/nimda/con/dto/LoginRequest.java
+++ NimdaConBackEnd/backend-spring/src/main/java/com/nimda/con/dto/LoginRequest.java
@@ -1,13 +1,16 @@
 package com.nimda.con.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public class LoginRequest {
     
     @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
     private String username;
     
     @NotBlank(message = "Password is required")
+    @Size(min = 3, max = 50, message = "Password must be between 3 and 100 characters")
     private String password;
     
     // 기본 생성자
