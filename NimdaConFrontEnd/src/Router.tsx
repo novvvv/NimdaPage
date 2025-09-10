5152bf5b6eea17547f6afa1698eced8838e9902d
nuyoes <s471y@kakao.com>
2025-09-10 20:48:37 +0900

    chore. create Router.tsx

---
+++ NimdaConFrontEnd/src/Router.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import App from "@/App";
+import LoginPage from "@/domains/User/Login/Page";
+
+const Router = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<App />} />
+                <Route path="/login" element={<LoginPage />} />
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default Router;
