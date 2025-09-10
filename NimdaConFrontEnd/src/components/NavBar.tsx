f287c082935895256637d7d349e08b005d8e196e
nov <novslog@gmail.com>
2025-09-10 18:39:01 +0900

    fix: navbar 반응형 디자인 수정 및 배경 추가

--- NimdaConFrontEnd/src/components/NavBar.tsx
+++ NimdaConFrontEnd/src/components/NavBar.tsx
@@ -4,8 +4,8 @@ import UserActions from './NavBar/UserActions';
 
 const NavBar: React.FC = () => {
   return (
-    <nav className="h-16 border-b border-[#E0E0E0]">
-      <div className="h-full px-4 flex items-center justify-between">
+    <nav className="fixed top-0 left-0 right-0 z-50 h-16 border-b border-[#E0E0E0] bg-white">
+      <div className="h-full px-4 xl:px-6 max-w-7xl mx-auto flex items-center justify-between">
         <BrandSection />
         <UserActions />
       </div>
