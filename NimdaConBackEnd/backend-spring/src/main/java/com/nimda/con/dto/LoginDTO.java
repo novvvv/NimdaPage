88c17d8926a8dc9d926d4ad1759dc5fed81ae1f6
nov <novslog@gmail.com>
2025-09-12 16:50:19 +0900

    REFACTOR: LoginDTO, RegisterDTO Refactoring

--- NimdaConBackEnd/backend-spring/src/main/java/com/nimda/con/dto/LoginDTO.java
+++ NimdaConBackEnd/backend-spring/src/main/java/com/nimda/con/dto/LoginDTO.java
@@ -2,7 +2,15 @@ package com.nimda.con.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginDTO {
     
     @NotBlank(message = "Username is required")
@@ -12,30 +20,4 @@ public class LoginDTO {
     @NotBlank(message = "Password is required")
     @Size(min = 3, max = 50, message = "Password must be between 3 and 100 characters")
     private String password;
-    
-    // 기본 생성자
-    public LoginDTO() {}
-    
-    // 생성자
-    public LoginDTO(String username, String password) {
-        this.username = username;
-        this.password = password;
-    }
-    
-    // Getters and Setters
-    public String getUsername() {
-        return username;
-    }
-    
-    public void setUsername(String username) {
-        this.username = username;
-    }
-    
-    public String getPassword() {
-        return password;
-    }
-    
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
--- NimdaConBackEnd/backend-spring/src/main/java/com/nimda/con/dto/RegisterDTO.java
+++ NimdaConBackEnd/backend-spring/src/main/java/com/nimda/con/dto/RegisterDTO.java
@@ -3,7 +3,15 @@ package com.nimda.con.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class RegisterDTO {
     
     @NotBlank(message = "Username is required")
@@ -17,39 +25,4 @@ public class RegisterDTO {
     @NotBlank(message = "Email is required")
     @Email(message = "Email should be valid")
     private String email;
-    
-    // 기본 생성자
-    public RegisterDTO() {}
-    
-    // 생성자
-    public RegisterDTO(String username, String password, String email) {
-        this.username = username;
-        this.password = password;
-        this.email = email;
-    }
-    
-    // Getters and Setters
-    public String getUsername() {
-        return username;
-    }
-    
-    public void setUsername(String username) {
-        this.username = username;
-    }
-    
-    public String getPassword() {
-        return password;
-    }
-    
-    public void setPassword(String password) {
-        this.password = password;
-    }
-    
-    public String getEmail() {
-        return email;
-    }
-    
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
