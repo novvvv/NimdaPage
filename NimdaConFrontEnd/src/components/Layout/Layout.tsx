ac4fc2a30f2e92e171be82f63ddf7c77f28f8aa0
nuyoes <s471y@kakao.com>
2025-09-24 19:24:11 +0900

    fix. main layout

--- NimdaConFrontEnd/src/components/Layout/Layout.tsx
+++ NimdaConFrontEnd/src/components/Layout/Layout.tsx
@@ -9,7 +9,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className="w-full">
       <NavBar />
-      <main className="pt-24 px-8 pb-8">{children}</main>
+      <main className="w=3/4 mx-auto pt-24 px-8 pb-8">{children}</main>
     </div>
   );
 };
