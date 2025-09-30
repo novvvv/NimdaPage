182157ea9cebae388d117335b742cdb572f8123f
nuyoes <s471y@kakao.com>
2025-09-30 23:17:24 +0900

    feat. add banner

--- NimdaConFrontEnd/src/domains/Home/index.tsx
+++ NimdaConFrontEnd/src/domains/Home/index.tsx
@@ -1,6 +1,6 @@
 import "@/App.css";
 import Layout from "@/components/Layout";
-import BlackLineButton from "@/components/Button/BlackLine";
+import Banner from "./components/banner";
 import { useNavigate } from "react-router-dom";
 
 import { getCurrentUsername, isAdmin } from "@/utils/jwt"; // 현재 로그인한 유저 출력
@@ -9,8 +9,9 @@ import { useState, useEffect } from "react";
 function Home() {
   return (
     <Layout>
-      <div className="flex flex-col items-center gap-4"></div>
-      home
+      <div className="flex flex-col items-center gap-4">
+        <Banner />
+      </div>
     </Layout>
   );
 }
