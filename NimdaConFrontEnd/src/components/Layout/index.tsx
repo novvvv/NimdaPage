3dc665567bab8a8b931f54d8dbd42139f74e345c
nuyoes <s471y@kakao.com>
2025-09-30 23:24:05 +0900

    fix. fix main container

--- NimdaConFrontEnd/src/components/Layout/index.tsx
+++ NimdaConFrontEnd/src/components/Layout/index.tsx
@@ -16,7 +16,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className="w-full">
       <NavBar menuItems={menuItems} />
-      <main className="w=3/4 mx-auto pt-24 px-8 pb-8">{children}</main>
+      <main className="w-full pb-8">{children}</main>
     </div>
   );
 };
