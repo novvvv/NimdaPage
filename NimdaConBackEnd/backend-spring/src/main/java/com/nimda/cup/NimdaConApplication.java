8806cfb3b8c49c0b71ffd0a14ab4fb798d48c211
Ju Yun Ho <ju020620@naver.com>
2025-12-12 16:55:41 +0900

    add controller scan

--- NimdaConBackEnd/backend-spring/src/main/java/com/nimda/cup/NimdaConApplication.java
+++ NimdaConBackEnd/backend-spring/src/main/java/com/nimda/cup/NimdaConApplication.java
@@ -3,11 +3,13 @@ package com.nimda.cup;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@ComponentScan(basePackages = { "com.nimda.cup", "com.nimda.cite" })  //  컨트롤러 스캔 추가
 @EntityScan(basePackages = { "com.nimda.cup.user.entity", "com.nimda.cup.judge.entity", "com.nimda.cup.word.entity",
         "com.nimda.cup.contest.entity", "com.nimda.cup.group.entity", "com.nimda.cite.board.entity" })
 @EnableJpaRepositories(basePackages = { "com.nimda.cup.user.repository", "com.nimda.cup.judge.repository",
