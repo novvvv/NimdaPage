3a606224b55f6c53d217af390e9a92a9f04d4275
nov <novslog@gmail.com>
2026-02-09 13:10:35 +0900

    test: NimdaCon Test Code Add

---
+++ NimdaConBackEnd/backend-spring/src/test/java/com/nimda/cup/NimdaConApplicationTests.java
@@ -0,0 +1,17 @@
+package com.nimda.cup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Spring Boot 애플리케이션 기본 테스트
+ * 애플리케이션이 정상적으로 시작되는지 확인합니다.
+ */
+@SpringBootTest
+class NimdaConApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // 애플리케이션 CI/CD Test
+    }
+}
