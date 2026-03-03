532a6ba7d2f410201923b913769d727df9c3a112
이명건 <audrjs987@naver.com>
2026-03-03 13:40:35 +0900

    fix: 댓글 dto 분리 및 사용자 확인 방식 변경

--- NimdaConBackEnd/backend-spring/src/main/java/com/nimda/cite/comment/dto/CommentResponse.java
+++ NimdaConBackEnd/backend-spring/src/main/java/com/nimda/cite/comment/dto/CommentResponse.java
@@ -1,50 +1,38 @@
 package com.nimda.cite.comment.dto;
 
-import com.nimda.cite.comment.entity.Comment;
-import com.nimda.cite.comment.enums.STATUS;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
-@Builder
+@NoArgsConstructor
 @AllArgsConstructor
-public class CommentResponse {
+@SuperBuilder // 빌더 상속을 위해 사용
+public abstract class CommentResponse {
+    protected Long id;
+    protected Long parentId;
+    protected String authorName;
+    protected String authorProfileImage;
+    protected String context;
+    protected String createdAt;
+    protected String updatedAt;
+    protected Integer likeCount;
+    protected Boolean isDeleted;
 
-    private Long id;
-    private Long parentId;
-    private Long boardId;
-    private String authorName;
-    private String context;
-    private Integer replyCount;
-    private STATUS status;
-    private LocalDateTime createdAt;
+    // 대댓글
+    @Builder.Default
+    protected List<CommentResponse> children = new ArrayList<>();
 
-    public static CommentResponse from(Comment comment, boolean isAdmin) {
-        String context;
+    public static final DateTimeFormatter DEFAULT_FORMATTER = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm");
 
-        if (isAdmin) {
-            context = comment.getContext();
-        } else {
-            context = switch (comment.getStatus()) {
-                case HIDDEN -> "숨긴 댓글입니다.";
-                case DELETED -> "삭제된 댓글입니다.";
-                default -> comment.getContext();
-            };
-        }
-
-        return CommentResponse.builder()
-                .id(comment.getId())
-                .parentId(comment.getParent() != null ? comment.getParent().getId() : null)
-                .boardId(comment.getBoard().getId())
-                .authorName(comment.getAuthor().getNickname())
-                .context(context)
-                .replyCount(comment.getReplyCount())
-                .status(comment.getStatus())
-                .createdAt(comment.getCreatedAt())
-                .build();
+    public static String formatDateTime(LocalDateTime dateTime) {
+        return dateTime != null ? dateTime.format(DEFAULT_FORMATTER) : null;
     }
-
 }
